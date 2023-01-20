474c4b92246f9ae71e3c5602650eec7986de2c9b
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-01-20 15:43:48 +0200

    Create 409. Longest Palindrome.js

---
+++ JavaScript/409. Longest Palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+        var ans = 0;
+        var n = s.length;
+        var count = 0;
+        const mp = new Map();
+        for(let i of s){
+            mp.set(i, mp.get(i) + 1 || 1);
+            if(mp.get(i)%2==0){
+                count=count+mp.get(i);
+                mp.set(i,0);
+            }
+        }
+        for(let i of s){
+            if(mp.get(i)%2==1){
+                count+=mp.get(i);
+                break;
+            }
+        }
+        return count;
+};
