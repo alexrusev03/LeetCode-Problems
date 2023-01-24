9f5a67d9d767fdde8bbb0c68f41983404bd20009
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-01-24 15:09:20 +0200

    Create 5. Longest Palindromic Substring.cpp

---
+++ C++/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int len=1;
+        int begin=0;
+        for(int i=1;i<=n;i++){
+            int l=i-1;
+            int r=i;
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(r-l+1>len){
+                    len=r-l+1;
+                    begin=l;
+                }
+                l--;
+                r++;
+            }
+            l=i-1;
+            r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(r-l+1>len){
+                    len=r-l+1;
+                    begin=l;
+                }
+                l--;
+                r++;
+            }
+        }
+        return s.substr(begin,len);
+    }
+};
