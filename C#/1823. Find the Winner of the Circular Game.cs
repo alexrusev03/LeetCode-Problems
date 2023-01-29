b0635a915679c35d35e5a479ab8fedfe0be4741f
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-01-29 15:06:04 +0200

    Create 1823. Find the Winner of the Circular Game.cs

---
+++ C#/1823. Find the Winner of the Circular Game.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int sol(int n, int k){
+        if(n==1){
+            return 0;
+        }
+        else{
+            return (sol(n-1,k)+k)%n;
+        }
+    }
+    public int FindTheWinner(int n, int k) {
+        return sol(n,k)+1;
+    }
+}
