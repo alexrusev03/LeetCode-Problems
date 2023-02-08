69567e815665ca3c36b3c8abcd733491427af37f
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-08 14:04:58 +0200

    Create 1137. N-th Tribonacci Number.cpp

---
+++ C++/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int tri[n+1];
+        if(n==0){return 0;}
+        if(n==1){return 1;}
+        if(n==2){return 1;}
+        tri[0]=0;
+        tri[1]=1;
+        tri[2]=1;
+        for(int i = 3; i<=n; i++){
+            tri[i]=tri[i-1]+tri[i-2]+tri[i-3];
+        }
+        return tri[n];
+    }
+};
