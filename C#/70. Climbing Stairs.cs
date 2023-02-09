884d4ea3ea9f61d820c033b1a55170ccfc1cf512
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-09 15:37:32 +0200

    Create 70. Climbing Stairs.cs

---
+++ C#/70. Climbing Stairs.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        int prev=1,prev2=1;
+        for(int i=2;i<=n;i++){
+            int cur=prev+prev2;
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;
+    }
+}
+//or
+
+public class Solution {
+    public int ClimbStairs(int n) {
+        int[] ways = new int[n+10];
+        ways[0]=0;
+        ways[1]=1;
+        ways[2]=2;
+        ways[3]=3;
+        for(int i = 4; i<=n; i++){
+            ways[i]=ways[i-1]+ways[i-2];
+        }
+        return ways[n];
+    }
+}
