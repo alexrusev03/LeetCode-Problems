ea9ffdd416b396b5925382751e176341a0bd5499
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-09 15:31:34 +0200

    Create 1407. Top Travellers.sql

---
+++ SQL Study Plan/1407. Top Travellers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select u.name, sum(ifnull(r.distance, 0)) as travelled_distance 
+from Users u
+left join Rides r
+on u.id = r.user_id
+group by u.name, u.id
+order by travelled_distance desc, u.name asc;
