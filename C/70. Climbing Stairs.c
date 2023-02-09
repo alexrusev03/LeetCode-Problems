3079421d8227071d3d56e17e276a766a3daa42e1
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-09 15:36:22 +0200

    Create 70. Climbing Stairs.c

---
+++ C/70. Climbing Stairs.c
@@ -0,0 +1,22 @@
+int climbStairs(int n){
+    int prev=1,prev2=1;
+    for(int i=2;i<=n;i++){
+        int cur=prev+prev2;
+        prev2=prev;
+        prev=cur;
+    }
+    return prev;
+}
+// or
+
+int climbStairs(int n){
+    int ways[n+10];
+    ways[0]=0;
+    ways[1]=1;
+    ways[2]=2;
+    ways[3]=3;
+    for(int i = 4; i<=n; i++){
+        ways[i]=ways[i-1]+ways[i-2];
+    }
+    return ways[n];
+}
