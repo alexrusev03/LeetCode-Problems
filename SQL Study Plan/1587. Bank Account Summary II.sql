55d0c80f2225f620cdd16112b5c5efe7d51a51d1
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-10 16:02:06 +0200

    Create 1587. Bank Account Summary II.sql

---
+++ SQL Study Plan/1587. Bank Account Summary II.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select u.name, sum(amount) as balance 
+from Users u 
+left join Transactions t 
+on u.account=t.account 
+group by u.account 
+having (sum(amount)>10000);
