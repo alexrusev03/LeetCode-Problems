8af4b7b9092f5396ea28d3dc498a4d7d6003eee0
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-14 16:51:22 +0200

    Create 33. Search in Rotated Sorted Array.cs

---
+++ C#/33. Search in Rotated Sorted Array.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int n = nums.Length;
+        int l = 0;
+        int r = n-1;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(nums[m]==target){return m;}
+            else if(nums[m]>=nums[l]){
+                if(nums[m]>target && nums[l]<=target){r=m-1;}
+                else{l=m+1;}
+            }
+            else{
+                if(nums[m]<target && nums[r]>=target){
+                    l=m+1;
+                }
+                else{
+                    r=m-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
