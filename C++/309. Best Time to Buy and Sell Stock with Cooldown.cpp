050e255daa3b994a6456fa9a1a379eaf75bc255b
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-18 17:59:02 +0200

    Create 309. Best Time to Buy and Sell Stock with Cooldown.cpp

---
+++ C++/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return helper(0,1,prices,dp);
+    }
+    int helper(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
+        if(i>=prices.size()){
+            return 0;
+        }
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+        if(buy!=0){
+            dp[i][buy]=max(-prices[i]+helper(i+1,0,prices,dp),helper(i+1,1,prices,dp));
+        }
+        else{
+           dp[i][buy]=max(prices[i]+helper(i+2,1,prices,dp),helper(i+1,0,prices,dp));
+        }
+        return dp[i][buy];
+    }
+};
