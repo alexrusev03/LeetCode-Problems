5350c934563cd725f307d4ab6008a3e9679f0044
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-21 15:03:12 +0200

    Create 264. Ugly Number II.cpp

---
+++ C++/264. Ugly Number II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        if(n <= 0){return 0;}
+        if(n == 1){return 1;}
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        vector<int> seq(n);
+        seq[0] = 1;
+        for(int i  = 1; i < n ; i ++){
+            seq[i] = min(seq[a]*2, min(seq[b]*3,seq[c]*5));
+            if(seq[i] == seq[a]*2) a++; 
+            if(seq[i] == seq[b]*3) b++;
+            if(seq[i] == seq[c]*5) c++;
+        }
+        return seq[n-1];
+    }
+};
