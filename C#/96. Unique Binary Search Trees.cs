6490edb768e7608c4446be40b842050773069bad
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-22 11:27:31 +0200

    Create 96. Unique Binary Search Trees.cs

---
+++ C#/96. Unique Binary Search Trees.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int NumTrees(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=dp[1]=1;
+        for (int i = 2; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
