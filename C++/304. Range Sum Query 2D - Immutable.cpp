2678d6a32cd13e9306810ac23718706cd8a0177b
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-26 12:37:02 +0200

    Create 304. Range Sum Query 2D - Immutable.cpp

---
+++ C++/304. Range Sum Query 2D - Immutable.cpp
@@ -0,0 +1,23 @@
+class NumMatrix {
+public:
+    vector<vector<int>> dp;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        dp = vector<vector<int>>(m+1,vector<int>(n+1,0));
+        for(int i = 1 ; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                    dp[i][j] = matrix[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+            }
+        }
+    }
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2+1][col2+1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+};
+
+/**
+ * Your Numdprix object will be instantiated and called as such:
+ * Numdprix* obj = new Numdprix(dprix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
