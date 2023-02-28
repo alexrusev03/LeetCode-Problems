cb9b8e2a0780a247b54367fa8d98199cebb9d014
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-02-28 11:18:30 +0200

    Create 63. Unique Paths II.java

---
+++ Java/63. Unique Paths II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        return helper(m-1,n-1,obstacleGrid,dp);
+    }
+    int helper(int m, int n, int[][] obstacleGrid, int[][] dp){
+        if(m<0 || n<0 || obstacleGrid[m][n]==1){
+            return 0;
+        }
+		if(m==0 && n==0){
+            return 1;
+        }
+        if(dp[m][n]!=0){
+            return dp[m][n];
+        }
+        int l=helper(m,n-1,obstacleGrid,dp);
+        int u=helper(m-1,n,obstacleGrid,dp);
+        return dp[m][n]=l+u;
+    }
+}
