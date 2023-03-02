3dc75caa21ac20e6c952e4f08a4e796dc5a765a7
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-02 11:56:13 +0200

    Create 221. Maximal Square.cpp

---
+++ C++/221. Maximal Square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int a;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    a = max(a, dp[i][j]);
+                }
+            }
+        }
+        return a*a;
+    }
+};
