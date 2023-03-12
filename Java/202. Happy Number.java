eba78e8c6b2ee2b7fef7213a9eda4f043ed6024e
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-12 11:52:27 +0200

    Create 202. Happy Number.java

---
+++ Java/202. Happy Number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n==1 || n==7){
+            return true;
+        }
+        if(n == 2 || n == 3 || n == 4 || n == 5 || n == 6 || n == 8 || n == 9){
+            return false;
+        }
+        int num=0;
+        while(n!=0){
+            num+=(n%10)*(n%10);
+            n/=10;
+        }
+        if(isHappy(num)==true){
+            return true;
+        }
+        return false;
+    }
+}
