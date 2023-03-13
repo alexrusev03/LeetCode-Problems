a90f8b19e6c0b65af276395ced49fdac43aa9a2a
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-13 11:08:36 +0200

    Create 279. Perfect Squares.c

---
+++ C/279. Perfect Squares.c
@@ -0,0 +1,19 @@
+int numSquares(int n){
+    if(n<4){return n;}
+    int dp[n+1];
+    for(int i = 0; i<n+1; i++){
+        dp[i] = INT_MAX;
+    }
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    for(int i=4;i<=n;i++){
+        for(int j=1;j<=sqrt(n);j++){
+            if(i>=j*j){
+                dp[i] = fmin(dp[i],1+dp[i-j*j]);
+            }
+        }
+    }
+    return dp[n];
+}
