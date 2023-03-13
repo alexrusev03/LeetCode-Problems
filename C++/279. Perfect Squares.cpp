f0885d5deff394e4ccc5df04a520d2c5a6f352ba
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-13 11:07:25 +0200

    Create 279. Perfect Squares.cpp

---
+++ C++/279. Perfect Squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        if(n<4){return n;}
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i=4;i<=n;i++){
+            for(int j=1;j<=sqrt(n);j++){
+                if(i>=j*j){
+                    dp[i] = min(dp[i],1+dp[i-j*j]);
+                }
+            }
+        }
+        return dp[n];
+    }
+};
