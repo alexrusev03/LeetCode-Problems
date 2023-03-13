e5a3a87fe0de439cf09ca9d5adffead9dbc07535
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-13 11:08:57 +0200

    Create 279. Perfect Squares.cs

---
+++ C#/279. Perfect Squares.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int NumSquares(int n) {
+        if(n<4){return n;}
+        int[] dp = new int[n+1];
+        for(int i = 0; i<n+1; i++){
+            dp[i] = Int32.MaxValue;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i=4;i<=n;i++){
+            for(int j=1;j<=Math.Sqrt(n);j++){
+                if(i>=j*j){
+                    dp[i] = Math.Min(dp[i],1+dp[i-j*j]);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
