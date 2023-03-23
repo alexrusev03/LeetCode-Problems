9c927b3945af8ccb0e5968fade77edd87f99dd3c
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-23 11:47:18 +0200

    Create 1. Two Sum.java

---
+++ Java/1. Two Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[j]==target-nums[i]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
