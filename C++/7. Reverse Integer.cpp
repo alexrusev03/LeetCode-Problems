d3678aebef5455739c57c852b91eb8d31dab3b25
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-27 10:45:27 +0300

    Create 7. Reverse Integer.cpp

---
+++ C++/7. Reverse Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long ans=0;
+        while(x!=0){
+            ans=ans*10+x%10;
+            x=x/10;
+        }
+        if(ans>INT_MAX || ans<-INT_MAX){
+            return 0;
+        }
+        return ans;
+    }
+};
