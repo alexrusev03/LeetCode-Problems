d8580b2a772f7e37a5d1a0fce7bf1671d59cfc17
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-03-27 10:45:48 +0300

    Create 7. Reverse Integer.java

---
+++ Java/7. Reverse Integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long ans=0;
+        while(x!=0){
+            ans=ans*10+x%10;
+            x=x/10;
+        }
+        if(ans>Integer.MAX_VALUE || ans<-Integer.MAX_VALUE){
+            return 0;
+        }
+        return (int)ans;
+    }
+}
