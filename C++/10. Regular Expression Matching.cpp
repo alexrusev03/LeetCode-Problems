d2bbe2da458f1da6d5c61b9669a01f94c9b7bed4
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-04-01 10:31:08 +0300

    Create 10. Regular Expression Matching.cpp

---
+++ C++/10. Regular Expression Matching.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for(int i=0; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(p[j-1] == '*'){
+                    dp[i][j] = dp[i][j-2] || (i != 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j]);
+                }
+                else if(i > 0 && (s[i-1] == p[j-1] || p[j-1] == '.')){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
