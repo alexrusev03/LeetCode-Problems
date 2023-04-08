ba2268a39743e4fd4aaf3d306352af4885f3b231
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-04-08 11:14:55 +0300

    Create 28. Find the Index of the First Occurrence in a String.c

---
+++ C/28. Find the Index of the First Occurrence in a String.c
@@ -0,0 +1,14 @@
+int strStr(char * haystack, char * needle){
+    int m=strlen(haystack);
+    int n=strlen(needle);
+    for(int i=0;i<m-n+1;i++){
+        int j=0;
+        while(j<n && haystack[i+j]==needle[j]){
+            j++;
+        }
+        if(j==n){
+            return i;
+        }
+    }
+    return -1;
+}
