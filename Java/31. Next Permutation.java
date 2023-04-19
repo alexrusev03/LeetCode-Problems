1655b9f09b9fbbc491f29fb194b53992d3eef809
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-04-19 12:01:58 +0300

    Create 31. Next Permutation.java

---
+++ Java/31. Next Permutation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length-1;
+        int j = nums.length-1;
+        while(i>0 && nums[i-1]>=nums[i]) {
+            i--;
+        }
+        if(i>0) {
+            while(nums[i-1]>=nums[j]){
+                j--;
+            }
+            swap(nums, i-1, j);
+        }
+        reverse(nums, i, nums.length-1);
+    }
+    public void swap(int[] nums, int i, int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+    public void reverse(int[] nums, int i, int j){
+        while(i<j){
+            swap(nums, i++, j--);
+        }
+    }
+}
