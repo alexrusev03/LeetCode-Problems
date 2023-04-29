298b71bb91284ea1562f0c49ef5777579914e993
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-04-29 10:19:04 +0300

    Create 57. Insert Interval.cpp

---
+++ C++/57. Insert Interval.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        vector<vector<int>> res;
+        for(int i = 0; i<n; i++) {
+            if(intervals[i][1] < newInterval[0]){
+                res.push_back(intervals[i]);
+            }
+            else if(newInterval[1] < intervals[i][0]){
+                res.push_back(newInterval);
+                newInterval=intervals[i];
+            }
+            else{
+                newInterval[0]=min(newInterval[0],intervals[i][0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
+            }    
+        }
+        res.push_back(newInterval);
+        return res;
+    }
+};
