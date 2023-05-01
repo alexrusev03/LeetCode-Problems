731d2b4218a5bc75051a66e61ee6095f3fd5be7e
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-05-01 11:19:19 +0300

    Create 73. Set Matrix Zeroes.java

---
+++ Java/73. Set Matrix Zeroes.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean flag = false;
+        for(int j = 0; j < n; j++){
+            if(matrix[0][j]==0){
+                flag = true;
+            }
+            for(int i = 1; i < m; i++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int j = 1; j < n; j++){
+            for(int i = 1; i < m; i++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if (flag==true){
+            for (int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
