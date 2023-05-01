7d31c0e8fbb3862c68c5fe13c950fedf920ce505
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-05-01 11:18:52 +0300

    Create 73. Set Matrix Zeroes.cpp

---
+++ C++/73. Set Matrix Zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool flag = false;
+        for(int j = 0; j < n; j++){
+            if(matrix[0][j]==0){
+                flag = true;
+            }
+            for(int i = 1; i < m; i++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int j = 1; j < n; j++){
+            for(int i = 1; i < m; i++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if (flag==true){
+            for (int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
