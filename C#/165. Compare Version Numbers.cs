76caa499d399baa7961f6d93d7906e76e5ea280d
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-05-22 21:07:20 +0300

    Create 165. Compare Version Numbers.cs

---
+++ C#/165. Compare Version Numbers.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int CompareVersion(string version1, string version2) {
+        int n1 = version1.Length;
+        int n2 = version2.Length;
+        int i=0;
+        int j=0;
+        long v1=0;
+        long v2=0;
+        while(i<n1 || j<n2){
+            while(i<n1 && version1[i]!='.'){
+                v1 = v1*10 + version1[i]-'0';
+                i++;
+            }
+            while(j<n2 && version2[j]!='.'){
+                v2 = v2*10 + version2[j]-'0';
+                j++;
+            }
+            if(v1<v2){
+                return -1;
+            }
+            if(v1>v2){
+                return 1;
+            }
+            i++;
+            j++;
+            v1=0;
+            v2=0;
+        }
+        return 0;
+    }
+}
