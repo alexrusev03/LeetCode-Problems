d3ccf4e8d495006c06191a0a720afb1d90d58935
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-08 10:46:23 +0300

    Create 326. Power of Three.js

---
+++ JavaScript/326. Power of Three.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n==1){
+        return true;
+    }
+    if(n%3==0 && n>0){
+        return isPowerOfThree(n/3);
+    }
+    return false;
+};
