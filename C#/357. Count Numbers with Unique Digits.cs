74fd989601664f8326db531a47cf36d3736d33e7
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-17 10:46:00 +0300

    Create 357. Count Numbers with Unique Digits.cs

---
+++ C#/357. Count Numbers with Unique Digits.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n) {
+        if(n==0){
+            return 1;
+        }
+        int[] arr = new int[n];
+        arr[0]=10;
+        int p=9;
+        int temp=9;
+        for(int i=1; i<n; i++){
+            p*=temp;
+            arr[i]=arr[i-1]+p;
+            temp--;
+        }
+        return arr[n-1];
+    }
+}
