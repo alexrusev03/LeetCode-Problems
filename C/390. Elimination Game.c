9be0409dd399144034a5320594a8ddfeab268511
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-23 11:18:00 +0300

    Create 390. Elimination Game.c

---
+++ C/390. Elimination Game.c
@@ -0,0 +1,15 @@
+int lastRemaining(int n){
+    bool l = true;
+    int res = 1;
+    int step = 1;
+    int rem = n;
+    while(rem > 1){
+        if(l || rem % 2 == 1){
+            res += step;
+        }
+        step *= 2;
+        rem /=  2;
+        l = !l;
+    }
+    return res;
+}
