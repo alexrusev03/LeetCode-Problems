720e21e52c00a3f7407ec124ebb58fc75440dc63
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-24 11:42:01 +0300

    Create 476. Number Complement.java

---
+++ Java/476. Number Complement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findComplement(int num) {
+        int t = num;
+        int p = 1;
+        while(t!=0){
+            t = t >> 1;
+            p = p << 1;
+        }
+        return (p-1)^num;
+    }
+}
