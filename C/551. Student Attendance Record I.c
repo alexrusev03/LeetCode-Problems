431a6fb7254f3526c207ea290c21d79123eb530e
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-25 10:42:28 +0300

    Create 551. Student Attendance Record I.c

---
+++ C/551. Student Attendance Record I.c
@@ -0,0 +1,23 @@
+bool checkRecord(char * s){
+    int a = 0;
+    int l = 0;
+    for(int i = 0; i<strlen(s); i++){
+        if(s[i]=='L'){
+            l++;
+        }
+        else if(s[i]=='A'){
+            l = 0;
+            a++;
+        }
+        else{
+            l=0;
+        }
+        if(l>=3){
+            return false;
+        }
+    }
+    if(a>=2){
+        return false;
+    }
+    return true;
+}
