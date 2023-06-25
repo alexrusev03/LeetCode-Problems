45dd76e26af4930597955e460cd6b14aeedd4a92
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-25 10:41:26 +0300

    Create 551. Student Attendance Record I.java

---
+++ Java/551. Student Attendance Record I.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int a = 0;
+        int l = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='L'){
+                l++;
+            }
+            else if(s.charAt(i)=='A'){
+                l = 0;
+                a++;
+            }
+            else{
+                l=0;
+            }
+            if(l>=3){
+                return false;
+            }
+        }
+        if(a>=2){
+            return false;
+        }
+        return true;
+    }
+}
