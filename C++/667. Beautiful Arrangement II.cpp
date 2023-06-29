6572a71027509fc109de69a0f5672371716154cd
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-29 11:08:34 +0300

    Create 667. Beautiful Arrangement II.cpp

---
+++ C++/667. Beautiful Arrangement II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+        vector<int> res(n);
+        int r = 0;
+        int d = k+1;
+        for(int i=0; i<k+1; i++){
+            if(i%2){
+                res[i]= d--;
+            }
+            else{
+                res[i] = ++r;
+            }
+        }
+        for(int i = k+1; i<n; i++){
+            res[i] = i+1;
+        }
+        return res;
+    }
+};
