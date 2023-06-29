0ac1d360f3daf3d51645468bf6fa79fe522d0d44
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-06-29 11:11:26 +0300

    Create 667. Beautiful Arrangement II.cs

---
+++ C#/667. Beautiful Arrangement II.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] ConstructArray(int n, int k) {
+        int[] res = new int[n];
+        int r = 0;
+        int d = k+1;
+        for(int i=0; i<k+1; i++){
+            if(i%2==1){
+                res[i]= d--;
+            }
+            else{
+                res[i] = ++r;
+            }
+        }
+        for(int i = k+1; i<n; i++){
+            res[i] = i+1;
+        }
+        return res;
+    }
+}
