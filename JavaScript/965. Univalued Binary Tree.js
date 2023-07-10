c412fdcd53322a26692951eaf178082b5d5f151a
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-07-10 11:43:50 +0300

    Create 965. Univalued Binary Tree.js

---
+++ JavaScript/965. Univalued Binary Tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+    if(root==null){
+        return true;
+    }
+    if(root.left!=null && root.left.val != root.val){
+        return false;
+    }
+    if(root.right!=null && root.right.val != root.val){
+        return false;
+    }
+    return isUnivalTree(root.left) && isUnivalTree(root.right);
+};
