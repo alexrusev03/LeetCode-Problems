8e22908f92f2301af5fc0923114c6e26ab703328
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-07-10 11:42:58 +0300

    Create 965. Univalued Binary Tree.c

---
+++ C/965. Univalued Binary Tree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isUnivalTree(struct TreeNode* root){
+    if(!root){
+        return true;
+    }
+    if(root->left && root->left->val != root->val){
+        return false;
+    }
+    if(root->right && root->right->val != root->val){
+        return false;
+    }
+    return isUnivalTree(root->left) && isUnivalTree(root->right);
+}
