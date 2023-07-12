90c58390aa2922304e9983751122ca005e7d5bfc
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-07-12 10:14:28 +0300

    Create 985. Sum of Even Numbers After Queries.cpp

---
+++ C++/985. Sum of Even Numbers After Queries.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int sum=0;
+        vector<int> res;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2==0){
+                sum += nums[i];
+            }
+        }
+        for(int i=0; i<queries.size(); i++){
+            if(nums[queries[i][1]]%2==0){
+                if((nums[queries[i][1]]+queries[i][0])%2!=0){
+                    sum -= nums[queries[i][1]];
+                }
+                if((nums[queries[i][1]]+queries[i][0])%2==0){
+                    sum += queries[i][0];
+                }
+            }
+            if(nums[queries[i][1]]%2!=0 && (nums[queries[i][1]]+queries[i][0])%2==0){
+                sum += nums[queries[i][1]]+queries[i][0];
+            }
+            nums[queries[i][1]] = nums[queries[i][1]]+queries[i][0];
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
