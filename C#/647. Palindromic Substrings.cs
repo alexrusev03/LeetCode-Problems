1b61f91b4c4e267cabf35fb28a9580d7371df44c
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-07-14 10:44:07 +0300

    Create 647. Palindromic Substrings.cs

---
+++ C#/647. Palindromic Substrings.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int CountSubstrings(string s) {
+        int res = 0;
+        int n = s.Length;
+        for(int i = 0; i<n; i++){
+            int j = i;
+            int k = i;
+            while(j>=0 && k<n && s[j] == s[k]){
+                j--;
+                k++;
+                res++;
+            }
+            j = i;
+            k = i+1;
+            while(j>=0 && k<n && s[j] == s[k]){
+                j--;
+                k++;
+                res++;
+            }
+        }
+        return res;
+    }
+}
