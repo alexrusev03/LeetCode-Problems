cb46622ada19e2c727760123e6ce1225ad4ae329
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-07-17 10:50:27 +0300

    Create 868. Binary Gap.cpp

---
+++ C++/868. Binary Gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int res = 0;
+        int l = -1;
+        for(int i = 0; i<32; i++){
+            if(n & (1 << i)){
+                if(l != -1){
+                    res = max(res, i - l);
+                }
+                l = i;
+            }
+        }
+        return res;
+    }
+};
