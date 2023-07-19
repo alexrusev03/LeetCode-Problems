a2bf42c2cc24e92181bd936656e8bea14b073020
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-07-19 11:20:09 +0300

    Create 814. Binary Tree Pruning.c

---
+++ C/814. Binary Tree Pruning.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* pruneTree(struct TreeNode* root){
+    if(root==NULL){
+        return NULL;
+    }
+    root->left = pruneTree(root->left);
+    root->right = pruneTree(root->right);
+    if(root->left==NULL && root->right==NULL && root->val == 0){
+        return NULL;
+    };
+    return root;
+}
