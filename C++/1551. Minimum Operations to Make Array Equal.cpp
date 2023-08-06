cef9f6e75422dd64306b852b095fce09bff3c368
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-08-06 10:28:27 +0300

    Create 1551. Minimum Operations to Make Array Equal.cpp

---
+++ C++/1551. Minimum Operations to Make Array Equal.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        int res=0;
+        for(int j=0; j<n/2; j++){
+            res+=n-2*j-1;
+        }
+        return res;
+    }
+};
