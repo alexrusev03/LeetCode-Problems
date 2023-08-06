38240a08d37c473094ef0a46bc7184057f1293b4
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-08-06 10:30:13 +0300

    Create 1551. Minimum Operations to Make Array Equal.cs

---
+++ C#/1551. Minimum Operations to Make Array Equal.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int MinOperations(int n) {
+        int res=0;
+        for(int j=0; j<n/2; j++){
+            res+=n-2*j-1;
+        }
+        return res;
+    }
+}
