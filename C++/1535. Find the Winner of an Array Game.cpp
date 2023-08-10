457dacc396b7015b0c16a251a8e678b1e1ddafd2
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-08-10 11:35:08 +0300

    Create 1535. Find the Winner of an Array Game.cpp

---
+++ C++/1535. Find the Winner of an Array Game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int n = arr.size();
+        int c = 0;
+        int max = 0;
+        int res = arr[0];
+        if(k > n){
+            for(int i = 0; i<n; i++){
+                if(arr[i]>max){
+                    max=arr[i];
+                }
+            }
+            return max;
+        }
+        for(int i=1; i<n; i++){
+            if(c==k){
+                break;
+            }
+            if(arr[i] > res){
+                res = arr[i];
+                c = 0;
+            }
+            c++;
+        }
+        return res;
+    }
+};
