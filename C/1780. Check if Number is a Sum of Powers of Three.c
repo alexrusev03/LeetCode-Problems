e55b72f569d2d338d1c31bcc5f953ba4abd45a98
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-08-31 12:02:20 +0300

    Create 1780. Check if Number is a Sum of Powers of Three.c

---
+++ C/1780. Check if Number is a Sum of Powers of Three.c
@@ -0,0 +1,17 @@
+bool checkPowersOfThree(int n){
+    int max = log(n)/log(3);
+    int res = 0;
+    for(int i = max; i>=0; i--){
+        if(res + pow(3,i) > n){
+            continue;
+        }
+        res += pow(3,i);
+        if(res==n){
+            return true;
+        }
+        if(res+pow(3,i)<n){
+            break;
+        }
+    }
+    return false;
+}
