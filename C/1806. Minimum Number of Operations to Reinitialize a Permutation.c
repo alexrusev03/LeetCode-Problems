5de257717b554946e5f26eb81e1c408ada9f0457
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-09-01 10:59:55 +0300

    Create 1806. Minimum Number of Operations to Reinitialize a Permutation.c

---
+++ C/1806. Minimum Number of Operations to Reinitialize a Permutation.c
@@ -0,0 +1,14 @@
+int reinitializePermutation(int n){
+    int res = 1;
+    int i = n/2;
+    while(i > 1){
+        if(i%2==0){
+            i /= 2;
+        }
+        else{
+            i = n/2+(i-1)/2;
+        }
+        res++;
+    }
+    return res;
+}
