a354550da30f2bd1966f93557dd4804f189b524e
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-09-12 12:11:03 +0300

    Create 1727. Largest Submatrix With Rearrangements.java

---
+++ Java/1727. Largest Submatrix With Rearrangements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int res = 0;
+        for(int i = 1; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] != 0){
+                    matrix[i][j] = matrix[i-1][j] + 1;
+                }
+            }
+        }
+        for(int i = 0; i < m; i++){
+            Arrays.sort(matrix[i]);
+            for(int j = 0; j < n; j++){
+                res = Math.max(res, matrix[i][j] * (n - j));
+            }
+        }
+        return res;
+    }
+}
