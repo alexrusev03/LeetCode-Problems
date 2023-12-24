2bc7d5a53b1638d8bbb6a470992b4edccba7067b
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2023-12-24 15:38:55 +0200

    Create 1053. Previous Permutation With One Swap.java

---
+++ Java/1053. Previous Permutation With One Swap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] prevPermOpt1(int[] arr) {
+        int n = arr.length;
+        int i = n-2;
+        int j = n-1;
+        while(i>=0 && arr[i+1]>=arr[i]){
+            i--;
+        }
+        if(i==-1){
+            return arr;
+        }
+        while((arr[j]>=arr[i] && j>i) || arr[j-1]==arr[j]){
+            j--;
+        }
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+        return arr;
+    }
+}
