46e5e9a09eeaf19f40e02b633a3ff0a162d11805
Alexandar Roussev <121508263+alexrusev03@users.noreply.github.com>
2024-07-31 17:17:47 +0300

    Create 1247. Minimum Swaps to Make Strings Equal.c

---
+++ C/1247. Minimum Swaps to Make Strings Equal.c
@@ -0,0 +1,16 @@
+int minimumSwap(char * s1, char * s2){
+    int xy = 0;
+    int yx = 0;
+    for(int i = 0 ; i < strlen(s1); i++){
+        if(s1[i] == 'x' && s2[i]=='y'){
+            xy++;
+        }
+        else if(s1[i] == 'y' && s2[i] == 'x'){
+            yx++;
+        }
+    }
+    if(xy%2 != yx%2){
+        return -1;
+    }
+    return xy/2 + yx/2 + (xy%2)*2;
+}
